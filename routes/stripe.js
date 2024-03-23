c286754997855de4b9dfb10554e3c36cf3258d39
Shivam Srivastava <shivam.srivastava3012@gmail.com>
2024-03-24 01:52:11 +0530

    small changes

--- routes/stripe.js
+++ routes/stripe.js
@@ -34,9 +34,9 @@ router.post("/create-checkout-session", async (req, res) => {
     metadata: {
       userId: req.body.userId,
       cart: JSON.stringify(req.body.cartItems),
-      shipping:req.body.shippingAmount,
-      discount:req.body.discountAmount
     },
+    shipping:JSON.stringify(req.body.shippingAmount),
+    discount:JSON.stringify(req.body.discountAmount)
   });
 
   const line_items = req.body.cartItems.map((item) => {
